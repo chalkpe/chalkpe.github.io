59c6fd614619ed5600f90555ecf778866b4a191d
ChalkPE <chalkpe@gmail.com>
2016-06-13 17:11:40 +0900

    First code drop

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#1ABD9D', '#E91E63', '#3F51B5', '#607D8B'],
+        anchors:['cover', 'dimigo', 'profile', 'contact']
+    });
+});
