acc4ef83e5d482afc10c664cc3954521215fbf82
ChalkPE <chalkpe@gmail.com>
2016-06-25 10:52:41 +0900

    Added scroll to top button and nav

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function(){
     $('#fullpage').fullpage({
+        anchors:['cover', 'dimigo', 'profile', 'contact'],
         sectionsColor: ['#1ABD9D', '#E91E63', '#3F51B5', '#607D8B'],
-        anchors:['cover', 'dimigo', 'profile', 'contact']
+
+        navigation: true, navigationPosition: 'right',
+        navigationTooltips: ['ChalkPE', 'Dimigo', 'Profile', 'Contact']
     });
+
+    $('#scroll-to-top').click(function(){
+        $.fn.fullpage.moveTo('cover');
+    })
 });
