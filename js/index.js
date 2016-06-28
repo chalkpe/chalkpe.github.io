4cd5c8d53d5c9f0890e59ef5d4d077645a606bfb
ChalkPE <chalkpe@gmail.com>
2016-06-28 12:55:46 +0900

    Many updates

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('#fullpage').fullpage({
         anchors:['cover', 'dimigo', 'profile', 'contact'],
-        sectionsColor: ['#1ABD9D', '#E91E63', '#3F51B5', '#607D8B'],
+        sectionsColor: ['#1ABD9D', '#E91E63', '#03a9f4', '#607D8B'],
 
         navigation: true, navigationPosition: 'right',
         navigationTooltips: ['ChalkPE', 'Dimigo', 'Profile', 'Contact'],
